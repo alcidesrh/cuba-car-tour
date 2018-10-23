97da8df5f4532fd68db7257e0e0efd3a10be26a0
Alcides <you@example.com>
2018-10-23 13:59:49 -0400

    Error en la llamada ajax

--- wp-content/themes/foundry-child/functions.php
+++ wp-content/themes/foundry-child/functions.php
@@ -28,16 +28,3 @@ function be_dps_template_part( $output, $original_atts ) {
     return $output;
 }
 add_action( 'display_posts_shortcode_output', 'be_dps_template_part', 10, 2 );
-
-//add_action( 'phpmailer_init', 'send_smtp_email' );
-//function send_smtp_email( $phpmailer ) {
-//    $phpmailer->isSMTP();
-//    $phpmailer->Host       = SMTP_HOST;
-////    $phpmailer->SMTPAuth   = SMTP_AUTH;
-//    $phpmailer->Port       = SMTP_PORT;
-////    $phpmailer->SMTPSecure = SMTP_SECURE;
-//    $phpmailer->Username   = SMTP_USERNAME;
-//    $phpmailer->Password   = SMTP_PASSWORD;
-////    $phpmailer->From       = SMTP_FROM;
-////    $phpmailer->FromName   = SMTP_FROMNAME;
-//}
